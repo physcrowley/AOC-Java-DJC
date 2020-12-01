000d84195495f91f8d52d4f42f17be5ab107b869
David Crowley <crowlda@ecolecatholique.ca>
2020-12-01 14:39:56 +0000

    Initial commit

---
+++ days/Day1/Day1.java
@@ -0,0 +1,13 @@
+public class Day1 extends Day {
+    Day1() {
+        inputFile = "days/Day1/input1.txt";
+    }
+
+    public void part1() {
+        // Good luck!
+    }
+
+    public void part2() {
+        
+    }
+}
