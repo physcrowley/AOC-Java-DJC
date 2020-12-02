78eb32a0a0f37bb284813ececf73304e373eb140
physcrowley <crowlda@ecolecatholique.ca>
2020-12-01 22:20:34 -0500

    tried to improve Day0.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
     System.out.print("\033[H\033[2J"); // escape character not working on Git Bash
     System.out.flush();  
     System.out.println("AOC 2020\n");
-    //(new Day0()).run();
+    (new Day0()).run();
     (new Day1()).run();
     (new Day2()).run();
     // (new Day3()).run();
--- days/Day0/Day0.java
+++ days/Day0/Day0.java
@@ -1,8 +1,12 @@
+import java.util.List;
+import java.util.LinkedList;
+
 public class Day0 extends Day {
     Day0() {
         inputFile = "days/Day0/input0.txt";
     }
 
+    private List<Integer> sumOfSquaresList = new LinkedList<Integer>();
     /**
      * 
      */
@@ -11,10 +15,25 @@ public class Day0 extends Day {
         //System.out.println(input);
         //for (byte c : input.getBytes()) System.out.println(c); //found CRLF line endings -> changed to LF
         int sumOfSquares = 0;
+        /* // original
         for (String line : input.split("\n")) {
             int num = Integer.parseInt(line);
             sumOfSquares += num * num;
         }
+        */
+        
+        // /* // with cache for part 2
+        int i = 0;
+        for (String line : input.split("\n")) {
+            int num = Integer.parseInt(line);
+            this.sumOfSquaresList.add(num * num);
+            sumOfSquares += this.sumOfSquaresList.get(i);
+            i++;
+        }
+        //System.out.println(sumOfSquaresList);
+        // */
+
+
         System.out.println(String.format("Sum of squares: %d", sumOfSquares));
     }
 
@@ -24,10 +43,22 @@ public class Day0 extends Day {
     public void part2() {
         String input = this.getInput();
         int sumOfCubes = 0;
+        /* // original
         for (String line : input.split("\n")) {
             int num = Integer.parseInt(line);
             sumOfCubes += num * num * num;
         }
+        */
+
+        // /* // with cache from part1
+        int i = 0;
+        for (String line : input.split("\n")) {
+            int num = Integer.parseInt(line);
+            sumOfCubes += this.sumOfSquaresList.get(i) * num;
+            i++;
+        }
+        // */
+
         System.out.println(String.format("Sum of cubes: %d", sumOfCubes));
     }
 }
