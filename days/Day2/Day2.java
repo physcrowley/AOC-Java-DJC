bd08e0ca3b52a6becf398492cfe11242737ad33a
physcrowley <crowlda@ecolecatholique.ca>
2020-12-01 21:50:48 -0500

    prepared Day2

---
+++ days/Day2/Day2.java
@@ -0,0 +1,24 @@
+public class Day2 
+{
+    public class Day2 extends Day {
+        Day2() {
+            inputFile = "days/Day2/input2.txt";
+        }
+    
+        /**
+         * 
+         */
+        public void part1() {
+
+            System.out.printf("\n");
+        }
+    
+        /**
+         * 
+         */
+        public void part2() {
+
+            System.out.printf("\n");
+        }
+    }
+}
