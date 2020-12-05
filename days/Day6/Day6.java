f6fe87cae3e4c8dcae05ebe6a14575d3f525ccf9
physcrowley <crowlda@ecolecatholique.ca>
2020-12-05 11:38:06 -0500

    prepping Day6

---
+++ days/Day6/Day6.java
@@ -0,0 +1,29 @@
+public class Day6 extends Day {
+    String[] lines;
+    Day6() {
+        inputFile = "days/Day6/input6.txt";
+        lines = getInput().split("\n");
+    }
+
+    /**
+     * 
+     */
+    public void part1() {   
+        for (String line : lines) {
+
+        }
+
+        System.out.printf("\n");
+    }
+
+    /**
+     * 
+     */
+    public void part2() {
+        for (String line : lines) {
+
+        }
+
+        System.out.printf("\n");
+    }
+}
